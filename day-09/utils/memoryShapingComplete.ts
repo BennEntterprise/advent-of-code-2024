9ab8718bcd48dffddd0c8d396e3571931bd563a6
Kyle Bennett <kylebennett00001@gmail.com>
2024-12-09 20:04:03 -0500

    day-09: optimizing memoryShapingComplete to be 0(n)

--- day-09/utils/memoryShapingComplete.ts
+++ day-09/utils/memoryShapingComplete.ts
@@ -1,40 +1,17 @@
 import { DiskMapBlocks } from "..";
 
 export function memoryShapingComplete(diskMapBlocks: DiskMapBlocks): boolean {
-  // Consume the map from both sides pushing the items into stacks,
-  // The left stack should always contain numbers, if not return false
-  // The right stack should always contain '.', if not return false.
-  // If you can continue such that left.length + right.length === diskMapBlocks.length, then return true.
-  const leftStack: number[] = [];
-  const rightStack: string[] = [];
+  let seenDot = false;
 
-  // Check to make sure the current characters can be pushed in...
   for (let i = 0; i < diskMapBlocks.length; i++) {
     if (typeof diskMapBlocks[i] === 'number') {
-      leftStack.push(diskMapBlocks[i] as number);
-      continue;
-    } else {
-      break;
+      if (seenDot) {
+        return false;
+      }
+    } else if (diskMapBlocks[i] === '.') {
+      seenDot = true;
     }
   }
 
-  for (let j = diskMapBlocks.length - 1; j >= 0; j--) {
-    if (diskMapBlocks[j] === '.') {
-      rightStack.unshift(diskMapBlocks[j] as string);
-      continue;
-    } else {
-      break;
-    }
-  }
-
-  // Update the Exhausted Map flag
-  // console.log({
-  //   leftStack: leftStack.join('' ),
-  //   rightStack: rightStack.join('')
-  // })
-  if (leftStack.length + rightStack.length === diskMapBlocks.length) {
-    return true;
-  } else {
-    return false;
-  }
+  return true;
 }
