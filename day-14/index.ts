f3cc108af69aaa40bf1afadae6b5ba05cd269ea4
Kyle Bennett <kylebennett00001@gmail.com>
2024-12-14 09:28:26 -0500

    day-14: solution to the sample problem

--- day-14/index.ts
+++ day-14/index.ts
@@ -30,21 +30,65 @@ try {
     // Advance robots
     console.clear()
     robots = advanceRobots(robots, room)
-    drawRobots(robots, room)
+    // drawRobots(robots, room)
     console.log(seconds)
     seconds++
-    await sleep(50)
+    await sleep(1)
   }
+
+  // Split room into quadrants
+  const xAxis = Math.floor(room.length / 2)
+  const yAxis = Math.floor(room[0].length /2)
+
+  const results = getQuadrantCounts(robots, xAxis,yAxis)
+  const multiplicativeResult = Object.values(results).reduce((acc, value) => {
+    return acc*=value
+  })
+  console.log({
+    results,
+    multiplicativeResult,
+    xAxis,
+    yAxis
+  })
 } catch (error) {
   console.error(error)
 }
 })()
 
 
+function getQuadrantCounts(robots: Robot[], xAxis: number, yAxis: number){
+  const quadrantCount = {
+    'I': 0,
+    'II': 0,
+    'III': 0,
+    'IV': 0
+  }
+  robots.forEach((robot) => {
+    if(
+      robot.position.y === xAxis
+      || robot.position.x === yAxis
+    ){
+      // Do nothing! This point is on an axis
+      return 
+    }
 
-
-
-function calculateWrapAround(){
-
+    if(robot.position.y > xAxis){
+      // Either Quadrant 3 or 4
+      if(robot.position.x < yAxis){
+        quadrantCount.III+=1
+      } else {
+        quadrantCount.IV+=1
+      }
+    } else if(robot.position.y < xAxis) {
+      // Either Quadrant 1 or 2
+      if(robot.position.x < yAxis){
+        quadrantCount.II+=1
+      } else {
+        quadrantCount.I+=1
+      }
+    } else {
+      throw Error('Should never happen.')
+    }
+  })
+  return quadrantCount
 }
-
